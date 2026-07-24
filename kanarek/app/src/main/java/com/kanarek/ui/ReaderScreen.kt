11bab0b0aa0c8383f110137896d956e7a569f0cb
trvny <120686325+trvny@users.noreply.github.com>
2026-07-24 09:34:35 +0200

    fix(kanarek): use scoped layout weight

--- kanarek/app/src/main/java/com/kanarek/ui/ReaderScreen.kt
+++ kanarek/app/src/main/java/com/kanarek/ui/ReaderScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
